448a3739addaa7cb8a7f43975aadd2b850dffaf6
Lalá Nametala <lalanametala@gmail.com>
2022-10-10 08:58:05 -0300

    :sparkles: feat(films): implementing the films page

---
+++ frontend-test-clubbi/src/pages/films/index.tsx
@@ -0,0 +1,63 @@
+import { Container, Grid } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import DisplayCard from '../../components/FilmCard'
+import FilterOrderForm from '../../components/FilterOrderForm'
+import { IFilm } from '../../interfaces/IFilm'
+import { fetchFilmsData } from '../../store/films'
+import { AppDispatch, RootState } from '../../store/store'
+
+export default function Films (): JSX.Element {
+  const useAppDispatch: () => AppDispatch = useDispatch
+  const initialFilms: IFilm[] = []
+  const [films, setFilms] = useState(initialFilms)
+  const dispatch = useAppDispatch()
+  const storedFilms = useSelector((state: RootState) => state.films.data)
+  const filmFilter = useSelector((state: RootState) => state.filters.films)
+
+  useEffect(() => {
+    if (!storedFilms.length) dispatch(fetchFilmsData())
+  }, [])
+
+  useEffect(() => {
+    setFilms(storedFilms)
+  }, [storedFilms])
+
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100vw',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <FilterOrderForm page="films" filterBy="Search by films" />
+      <Grid
+        container
+        component="section"
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          alignSelf: 'center',
+          width: '90vw',
+          marginBottom: { xs: '50px', sm: '50px', md: '120px' },
+          justifyContent: 'center',
+          alignItems: 'top',
+          gap: '20px'
+        }}
+        columns={{ xs: 4, sm: 4, md: 12 }}
+      >
+        {films
+          .filter(({ title }) => title.toLowerCase().includes(filmFilter.toLowerCase()))
+          .map((item) => (
+            <Grid key={item.id} item>
+              <DisplayCard {...item} />
+            </Grid>
+        ))}
+      </Grid>
+    </Container>
+  )
+}
