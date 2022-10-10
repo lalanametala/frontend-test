338fac9036663123962368bb25b7f172ace25c3b
Lalá Nametala <lalanametala@gmail.com>
2022-10-10 08:43:48 -0300

    :sparkles: feat(redux): filters reducer

---
+++ frontend-test-clubbi/src/store/filters.tsx
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const INITIAL_STATE = {
+  films: '',
+  characters: '',
+  locations: ''
+}
+
+const filtersSlice = createSlice({
+  name: 'filters',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setFilmFilter (state, { payload }) {
+      state.films = payload
+    },
+    setCharacterFilter (state, { payload }) {
+      state.characters = payload
+    },
+    setLocationFilter (state, { payload }) {
+      state.locations = payload
+    },
+    resetAllFilters (state) {
+      state.films = INITIAL_STATE.films
+      state.characters = INITIAL_STATE.characters
+      state.locations = INITIAL_STATE.locations
+    }
+  }
+})
+
+export const { setFilmFilter, setCharacterFilter, setLocationFilter, resetAllFilters } = filtersSlice.actions
+
+export default filtersSlice.reducer
