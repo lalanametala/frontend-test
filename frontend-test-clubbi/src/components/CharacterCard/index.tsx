b4c594d361fd3b4018214d3e3c7f42355a11538a
Lalá Nametala <lalanametala@gmail.com>
2022-10-10 16:57:30 -0300

    :sparkles: feat(CharacterCard): displaying films

--- frontend-test-clubbi/src/components/CharacterCard/index.tsx
+++ frontend-test-clubbi/src/components/CharacterCard/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@mui/material'
 import useElevation from '../../hooks'
 import { ICharacter } from '../../interfaces/ICharacter'
+import { AppDispatch, RootState } from '../../store/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { IFilm } from '../../interfaces/IFilm'
+import { fetchFilmsData } from '../../store/films'
 
 export default function DisplayCard (character: ICharacter): JSX.Element {
   const { elevation, handleMouseOver, handleMouseOut } = useElevation()
+  const storedFilms = useSelector((state: RootState) => state.films.data)
+  const useAppDispatch: () => AppDispatch = useDispatch
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (!storedFilms.length) dispatch(fetchFilmsData())
+  }, [])
 
   return (
     <Card 
@@ -27,9 +38,17 @@ export default function DisplayCard (character: ICharacter): JSX.Element {
           {`Hair color: ${character.hair_color}`}
         </Typography>
       </CardContent>
-      <CardContent sx={{ paddingTop: 0 }}>
+      <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
           {`Eye color: ${character.eye_color}`}
+        </Typography>
+      </CardContent>
+      <CardContent sx={{ paddingTop: 0 }}>
+        <Typography>
+          {`Films:${character.films.map((film) => {
+            const filmInfo = storedFilms.find(({url}) => film === url) as IFilm
+            return ` ${filmInfo.title}`
+          })}`}
         </Typography>
       </CardContent>
     </Card>
