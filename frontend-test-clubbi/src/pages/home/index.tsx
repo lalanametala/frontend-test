7428386c3946cefad4aaa1fba6cb2de28ba2b7d8
Lalá Nametala <lalanametala@gmail.com>
2022-10-10 08:53:49 -0300

    :sparkles: feat(home): implementing the homepage

---
+++ frontend-test-clubbi/src/pages/home/index.tsx
@@ -0,0 +1,47 @@
+import { Grid } from '@mui/material'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import StudioLogo from '../../components/StudioGhibliLogo'
+import { fetchCharactersData } from '../../store/characters'
+import { fetchFilmsData } from '../../store/films'
+import { fetchLocationsData } from '../../store/locations'
+import { AppDispatch, RootState } from '../../store/store'
+import About from './sections/AboutStudioGhibli'
+
+export default function Home (): JSX.Element {
+  const theme = useSelector((state: RootState) => state.theme.data)
+  const useAppDispatch: () => AppDispatch = useDispatch
+  const dispatch = useAppDispatch()
+  const films = useSelector((state: RootState) => state.films.data)
+  const characters = useSelector((state: RootState) => state.characters.data)
+  const locations = useSelector((state: RootState) => state.locations.data)
+
+  useEffect(() => {
+    if (!films.length) dispatch(fetchFilmsData())
+    if (!characters.length) dispatch(fetchCharactersData())
+    if (!locations.length) dispatch(fetchLocationsData())
+  }, [])
+
+  return (
+    <Grid
+      container
+      component="section"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        paddingX: { xs: '70px', sm: '70px', md: '90px' },
+        width: '100vw',
+        marginY: { xs: '50px', sm: '50px', md: '120px' },
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        gap: '100px'
+      }}
+      columns={{ xs: 4, sm: 4, md: 12 }}
+    >
+      <Grid item>
+        <StudioLogo logoMargin="0" logoHeight="200px" logoPadding="0px" dispXS="none" dispSM="none" dispMD="block" pgTheme={theme} />
+      </Grid>
+      <About />
+    </Grid>
+  )
+}
---
+++ frontend-test-clubbi/src/pages/home/sections/AboutStudioGhibli/index.tsx
@@ -0,0 +1,62 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+
+export default function About (): JSX.Element {
+  return (
+    <Grid
+      item
+      columns={{ xs: 4, sm: 4, md: 4 }}
+      component="section"
+      sx={{
+        marginLeft: '10%',
+        width: { xs: '100%', sm: '80%', md: '80%' }
+      }}
+    >
+      <Grid
+        item
+        display={{ xs: 'block', sm: 'block', md: 'none' }}
+        columns={{ xs: 4, sm: 4, md: 2 }}
+      >
+        <Typography
+          variant="h4"
+          component="h2"
+          align="left"
+          gutterBottom
+          sx={{ fontFamily: '"Sniglet", cursive', fontWeight: 700, paddingBottom: '20px', paddingTop: '50px' }}
+        >
+          Studio Ghibli
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        columns={{ xs: 4, sm: 4, md: 2 }}
+      >
+        <Typography
+          align="left"
+          paragraph
+          sx={{ fontSize: { xs: '20px', sm: '24px', md: '20px' }, paddingY: '10px' }}
+        >
+          Studio Ghibli is a Japanese animation studio based in Koganei, Tokyo.
+          Founded in 1985, the studio has produced 21 animated features, the
+          first being Castle in the Sky (1986) and the most recent When Marnie Was There (2014).
+        </Typography>
+        <Typography
+          align="left"
+          paragraph
+          sx={{ fontSize: { xs: '20px', sm: '24px', md: '20px' }, paddingY: '10px', paddingBottom: '70px' }}
+        >
+          Studio Ghibli was founded in 1985 by Hayao Miyazaki, Isao Takahata,
+          Toshio Suzuki and Yasuyoshi Tokuma, shortly after the success of
+          Nausicaä of the Valley of the Wind the previous year.
+          The studio released its first film, Castle in the Sky, the following year.
+          The company`s logo is Totoro, the character from the movie
+          My Neighbor Totoro, released in 1988.
+          With the exception of six productions,
+          all of the studio`s films were directed by Hayao Miyazaki and Isao Takahata.
+          Toshio Suzuki, meanwhile, is the producer for most of them.
+          In 2001, the Ghibli Museum, a museum dedicated to the studio`s works, opened.
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
