9bb343dabf2a0447c37b7f1088e76454c72b8d8c
Lalá Nametala <lalanametala@gmail.com>
2022-10-12 22:23:42 -0300

    :bug: fix(LocationCard): fixing lint errors

--- frontend-test-clubbi/src/components/LocationCard/index.tsx
+++ frontend-test-clubbi/src/components/LocationCard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@mui/material'
+import { useSelector } from 'react-redux'
 import useElevation from '../../hooks'
 import { ILocation } from '../../interfaces/ILocation'
-import { useSelector } from 'react-redux'
 import { RootState } from '../../store/store'
 import { IFilm } from '../../interfaces/IFilm'
 import { ICharacter } from '../../interfaces/ICharacter'
@@ -11,52 +11,55 @@ export default function DisplayCard (location: ILocation): JSX.Element {
   const { elevation, handleMouseOver, handleMouseOut } = useElevation()
   const storedFilms = useSelector((state: RootState) => state.films.data)
   const storedCharacters = useSelector((state: RootState) => state.characters.data)
+  const { name, climate, terrain, surface_water: surfaceWater, films, residents } = location
 
   return (
-    <Card 
+    <Card
       elevation={elevation}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
-      sx={{ width: { xs: '300px', sm: '400px', md: '400px'},
-        height: { xs: '300px', sm: '400px', md: '280px'}
+      sx={{
+        width: { xs: '300px', sm: '400px', md: '400px' },
+        height: { xs: '300px', sm: '400px', md: '280px' }
       }}
     >
       <CardHeader
-        title={location.name}
+        title={name}
       />
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Climate: ${location.climate}`}
+          {`Climate: ${climate}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Terrain: ${location.terrain}`}
+          {`Terrain: ${terrain}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Surface water: ${location.surface_water}`}
+          {`Surface water: ${surfaceWater}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Films:${location.films.map((film) => {
-            if (!film || !storedFilms.length) return null
-            const filmInfo = storedFilms.find(({url}) => film === url) as IFilm
+          {`Films:${films.map((film) => {
+            if (!film || !storedFilms.length) return ''
+            const filmInfo = storedFilms.find(({ url }) => film === url) as IFilm
             return ` ${filmInfo.title}`
-          })}`}
+          }).toString()}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0 }}>
         <Typography>
-          {`Residents:${location.residents.map((character) => {
-            if (!character || !storedCharacters.length) return null
+          {`Residents:${residents.map((character) => {
+            if (!character || !storedCharacters.length) return ''
             if (character === 'TODO') return ' TODO'
-            const characterInfo = storedCharacters.find(({url}) => character === url) as ICharacter
-            
+            const characterInfo = storedCharacters
+              .find(({ url }) => character === url) as ICharacter
+
             return ` ${characterInfo.name}`
-          })}`}
+          }).toString()}`}
         </Typography>
       </CardContent>
     </Card>
