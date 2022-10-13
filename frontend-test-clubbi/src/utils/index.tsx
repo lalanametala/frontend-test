e31e61a19423189dcadc341aec7f85c55b852aa7
Lalá Nametala <lalanametala@gmail.com>
2022-10-12 21:18:38 -0300

    :bug: fix(utils): fixing lint errors

--- frontend-test-clubbi/src/utils/index.tsx
+++ frontend-test-clubbi/src/utils/index.tsx
@@ -1,118 +1,115 @@
-import { ICharacter } from "../interfaces/ICharacter";
-import { IFilm } from "../interfaces/IFilm";
-import { ILocation } from "../interfaces/ILocation";
-import { characterParams, filmParams, locationParams } from "../store/sort";
+import { ICharacter } from '../interfaces/ICharacter'
+import { IFilm } from '../interfaces/IFilm'
+import { ILocation } from '../interfaces/ILocation'
+import { characterParams, filmParams, locationParams } from '../store/sort'
 
-type mappedData = {
+type mappedDataType = {
   value: string | number
   index: number
 }
 
-export const ascSorterString = (a: mappedData, b: mappedData) => {
+export const ascSorterString = (a: mappedDataType, b: mappedDataType): number => {
   if (a.value > b.value) {
-    return 1;
+    return 1
   }
   if (a.value < b.value) {
-    return -1;
+    return -1
   }
-  return 0;
-};
+  return 0
+}
 
-export const descSorterString = (a: mappedData, b: mappedData) => {
+export const descSorterString = (a: mappedDataType, b: mappedDataType): number => {
   if (a.value < b.value) {
-    return 1;
+    return 1
   }
   if (a.value > b.value) {
-    return -1;
+    return -1
   }
-  return 0;
-};
+  return 0
+}
 
-export const ascSorterNumber = (a: mappedData, b: mappedData) => {
+export const ascSorterNumber = (a: mappedDataType, b: mappedDataType): number => {
   if (+a.value > +b.value) {
-    return 1;
+    return 1
   }
   if (+a.value < +b.value) {
-    return -1;
+    return -1
   }
-  return 0;
-};
+  return 0
+}
 
-export const descSorterNumber = (a: mappedData, b: mappedData) => {
+export const descSorterNumber = (a: mappedDataType, b: mappedDataType): number => {
   if (+a.value < +b.value) {
-    return 1;
+    return 1
   }
   if (+a.value > +b.value) {
-    return -1;
+    return -1
   }
-  return 0;
-};
+  return 0
+}
 
-export const sortFilms = (data: IFilm[], order: filmParams ) => {
-  const { parameter, sort } = order;
+export const sortFilms = (data: IFilm[], order: filmParams): IFilm[] => {
+  const { parameter, sort } = order
   const mappedData = data.map((item, index) => ({
     value: item[parameter],
-    index,
-  }));
+    index
+  }))
 
-  if(parameter === 'title') {
-    if (sort === 'asc') mappedData.sort(ascSorterString);
-    if (sort === 'desc') mappedData.sort(descSorterString);
-    const sortedData = mappedData.map(({ index }) => data[index]);
-    return sortedData;
-  } else {
-    const knownData = mappedData.filter((item) => !isNaN(Number(item.value)));
-    
-    if (sort === 'asc') knownData.sort(ascSorterNumber);
-    if (sort === 'desc') knownData.sort(descSorterNumber);
-    const sortedData = knownData.map(({ index }) => data[index]);
-    const unknownData = data.filter((item) => isNaN(Number(item[parameter])));
-    return [...sortedData, ...unknownData];  
+  if (parameter === 'title') {
+    if (sort === 'asc') mappedData.sort(ascSorterString)
+    if (sort === 'desc') mappedData.sort(descSorterString)
+    const sortedData = mappedData.map(({ index }) => data[index])
+    return sortedData
   }
-};
+  const knownData = mappedData.filter((item) => !Number.isNaN(Number(item.value)))
 
-export const sortCharacters = (data: ICharacter[], order: characterParams ) => {
-  const { parameter, sort } = order;
+  if (sort === 'asc') knownData.sort(ascSorterNumber)
+  if (sort === 'desc') knownData.sort(descSorterNumber)
+  const sortedData = knownData.map(({ index }) => data[index])
+  const unknownData = data.filter((item) => Number.isNaN(Number(item[parameter])))
+  return [...sortedData, ...unknownData]
+}
+
+export const sortCharacters = (data: ICharacter[], order: characterParams): ICharacter[] => {
+  const { parameter, sort } = order
   const mappedData = data.map((item, index) => ({
     value: item[parameter],
-    index,
-  }));
+    index
+  }))
 
-  if(parameter === 'name') {
-    if (sort === 'asc') mappedData.sort(ascSorterString);
-    if (sort === 'desc') mappedData.sort(descSorterString);
-    const sortedData = mappedData.map(({ index }) => data[index]);
-    return sortedData;
-  } else {
-    const knownData = mappedData.filter((item) => !isNaN(Number(item.value)));
-    
-    if (sort === 'asc') knownData.sort(ascSorterNumber);
-    if (sort === 'desc') knownData.sort(descSorterNumber);
-    const sortedData = knownData.map(({ index }) => data[index]);
-    const unknownData = data.filter((item) => isNaN(Number(item[parameter])));
-    return [...sortedData, ...unknownData];  
+  if (parameter === 'name') {
+    if (sort === 'asc') mappedData.sort(ascSorterString)
+    if (sort === 'desc') mappedData.sort(descSorterString)
+    const sortedData = mappedData.map(({ index }) => data[index])
+    return sortedData
   }
-};
+  const knownData = mappedData.filter((item) => !Number.isNaN(Number(item.value)))
 
-export const sortLocations = (data: ILocation[], order: locationParams ) => {
-  const { parameter, sort } = order;
+  if (sort === 'asc') knownData.sort(ascSorterNumber)
+  if (sort === 'desc') knownData.sort(descSorterNumber)
+  const sortedData = knownData.map(({ index }) => data[index])
+  const unknownData = data.filter((item) => Number.isNaN(Number(item[parameter])))
+  return [...sortedData, ...unknownData]
+}
+
+export const sortLocations = (data: ILocation[], order: locationParams): ILocation[] => {
+  const { parameter, sort } = order
   const mappedData = data.map((item, index) => ({
     value: item[parameter],
-    index,
-  }));
+    index
+  }))
 
-  if(parameter === 'name') {
-    if (sort === 'asc') mappedData.sort(ascSorterString);
-    if (sort === 'desc') mappedData.sort(descSorterString);
-    const sortedData = mappedData.map(({ index }) => data[index]);
-    return sortedData;
-  } else {
-    const knownData = mappedData.filter((item) => !isNaN(Number(item.value)));
-    
-    if (sort === 'asc') knownData.sort(ascSorterNumber);
-    if (sort === 'desc') knownData.sort(descSorterNumber);
-    const sortedData = knownData.map(({ index }) => data[index]);
-    const unknownData = data.filter((item) => isNaN(Number(item[parameter])));
-    return [...sortedData, ...unknownData];  
+  if (parameter === 'name') {
+    if (sort === 'asc') mappedData.sort(ascSorterString)
+    if (sort === 'desc') mappedData.sort(descSorterString)
+    const sortedData = mappedData.map(({ index }) => data[index])
+    return sortedData
   }
-};
+  const knownData = mappedData.filter((item) => !Number.isNaN(Number(item.value)))
+
+  if (sort === 'asc') knownData.sort(ascSorterNumber)
+  if (sort === 'desc') knownData.sort(descSorterNumber)
+  const sortedData = knownData.map(({ index }) => data[index])
+  const unknownData = data.filter((item) => Number.isNaN(Number(item[parameter])))
+  return [...sortedData, ...unknownData]
+}
