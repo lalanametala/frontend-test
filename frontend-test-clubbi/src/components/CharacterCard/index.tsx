420b2a419f9df743b154f9c85eae2a38567961d1
Lalá Nametala <lalanametala@gmail.com>
2022-10-12 22:17:46 -0300

    :bug: fix(Character Card): fixing lint errors

--- frontend-test-clubbi/src/components/CharacterCard/index.tsx
+++ frontend-test-clubbi/src/components/CharacterCard/index.tsx
@@ -1,48 +1,49 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@mui/material'
+import { useSelector } from 'react-redux'
 import useElevation from '../../hooks'
 import { ICharacter } from '../../interfaces/ICharacter'
 import { RootState } from '../../store/store'
-import { useSelector } from 'react-redux'
 import { IFilm } from '../../interfaces/IFilm'
 
 export default function DisplayCard (character: ICharacter): JSX.Element {
   const { elevation, handleMouseOver, handleMouseOut } = useElevation()
   const storedFilms = useSelector((state: RootState) => state.films.data)
+  const { name, age, gender, hair_color: hairColor, eye_color: eyeColor, films } = character
 
   return (
-    <Card 
+    <Card
       elevation={elevation}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
       sx={{ width: { xs: '300px', sm: '400px', md: '450px' } }}
     >
       <CardHeader
-        title={character.name}
-        subheader={`Age: ${character.age}`}
+        title={name}
+        subheader={`Age: ${age}`}
       />
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Gender: ${character.gender}`}
+          {`Gender: ${gender}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Hair color: ${character.hair_color}`}
+          {`Hair color: ${hairColor}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0, paddingBottom: '5px' }}>
         <Typography>
-          {`Eye color: ${character.eye_color}`}
+          {`Eye color: ${eyeColor}`}
         </Typography>
       </CardContent>
       <CardContent sx={{ paddingTop: 0 }}>
         <Typography>
-          {`Films:${character.films.map((film) => {
-            if (!film || !storedFilms.length) return null
-            const filmInfo = storedFilms.find(({url}) => film === url) as IFilm
+          {`Films:${films.map((film) => {
+            if (!film || !storedFilms.length) return ''
+            const filmInfo = storedFilms.find(({ url }) => film === url) as IFilm
             return ` ${filmInfo.title}`
-          })}`}
+          }).toString()}`}
         </Typography>
       </CardContent>
     </Card>
