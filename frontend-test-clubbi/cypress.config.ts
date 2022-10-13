0f109383b41370b46d4da05870e2bf0cacf0bcc5
Lalá Nametala <lalanametala@gmail.com>
2022-10-12 22:59:30 -0300

    :bug: fix(cypress config): fixing lint errors

--- frontend-test-clubbi/cypress.config.ts
+++ frontend-test-clubbi/cypress.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   viewportWidth: 1400,
   viewportHeight: 800,
 
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000',
     video: false,
     slowTestThreshold: 15000,
-    testIsolation: "legacy",
+    testIsolation: 'legacy',
     defaultCommandTimeout: 20000
-  },
-});
+  }
+})
